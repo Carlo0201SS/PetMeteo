adf49c4499cab5605374c86dde9df1eaf86d6bc2
Carlo <carloabis0201@gmail.com>
2025-11-14 12:22:20 +0100

    Sistemazione dello UserScreenViewModel con generationtext richiamato dal PromptService che gestisce i prompt utilizzati nell'app.

--- lib/UI/scenes/userScreen/ViewModel/UserScreenViewModel.dart
+++ lib/UI/scenes/userScreen/ViewModel/UserScreenViewModel.dart
@@ -424,11 +424,11 @@ class HomePageViewModel extends ChangeNotifier {
         }
       }
 
-PromptService prompt = PromptService();
+PromptService promptclassifier = PromptService();
 
       _llmRequest1 = LLMRequest1(
         model: "gpt-3.5-turbo",
-        messages: [LLMMessage1(role: "user", content: prompt.getClassifierTextPrompt(temperatura, vento, currentCode, currentProbPrecipitation, isBadWeather))],
+        messages: [LLMMessage1(role: "user", content: promptclassifier.getClassifierTextPrompt(temperatura, vento, currentCode, currentProbPrecipitation, isBadWeather))],
         maxTokens: 4096,
         temperature: 1,
         topP: 1,
@@ -456,41 +456,37 @@ PromptService prompt = PromptService();
         isBadWeather = false;
       }
 
-      final prompt2 =
-          """
-Sei PetMeteo, un meteorologo empatico e allegro che ama gli animali e l'aria aperta! 
-Riceverai in input questi parametri:
-- temperatura (°C)
-- condizioni del cielo
-- velocità del vento (km/h)
-- probabilità di precipitazione (%)
+      PromptService promptgenerationtext = PromptService();
+      
+      
 
-Ti darò anche il nome dell'animale, mi raccomando è il nome dell'animale, non il mio! Parla come se stessi parlando con me e il mio cucciolo [nome del cucciolo].
-Esempi: 'Oggi è una bella giornata per passeggiare con [nome cucciolo]! Approfittatene e passate un buon momento insieme!', 'Giornata incerta! Valuta se uscire con [nome cucciolo]'
-'Brutta giornata, ma tra un'ora si libera il cielo, ora stai a casa con [nome cucciolo] più tardi potrai portarlo a spasso dove vuoi!'
-'Oggi è una bellissima giornata di sole, porta il tuo [nome cucciolo] a correre al parco, vi auguro una serena paseggiata!'
-'Che tempesta di pioggia, meglio rimanere a casa con [nome cucciolo], vi propongo di giocare con la palla nel vostro soggiorno, così può svagarsi anche se fuori fa brutto tempo. Enjoy it!'
-Come haiu potuto vedere, il tuo compito è creare una frase di 50 parole dal tono vivace , dove specifichi il nome del cucciolo (ricorda quel nome è del cucciolo, non il mio! Il mio non lo devi fare, anche perchè non lo sai e non lo saprai mai!) e 
-mi devi dire se si può uscire o meno in base alle condizioni fornite, rendi la frase leggera senza fronzoli.
 
-- Se le condizioni sono miti e gradevoli, invita con entusiasmo a godersi una bella passeggiata all'aperto, se invece le condizioni sono cattive(in base ai dati che ti do) scoonsiglia vivamente l'uscita e proponi qualche attività al chiuso.
-- Cerca di scrivere una frase che non contenga i valori numerici dei dati meteorologici, ma soltanto una bella frase da mostrare e nel caso di mancata uscita proponi un'attività da fare con l'amato cucciolo.
-Evita frasi ripetitive e varia la struttura delle frasi, non menzionare i valori numerici
-Inoltre non inventarti nulla di sana pianta,per nessun motivo, rispetta sempre le regole come so che tu sai fare, conto su di te!
 
-Dati in Input:
-Temperatura: ${temperatura.toStringAsFixed(1)}°, 
-Cielo: ${_getWeatherString(currentCode)}, 
-Velocità vento: ${vento.toStringAsFixed(1)} km/h,
-Probabilità precipitazione: ${currentProbPrecipitation.toString()}
-Nome animale: ${text.toString()}
-isBadWeather: $isBadWeather
 
-""";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
       _llmRequest2 = LLMRequest2(
         model: "gpt-3.5-turbo",
-        messages: [LLMMessage2(role: "user", content: prompt2)],
+        messages: [LLMMessage2(role: "user", content: promptgenerationtext.getGenerationTextPrompt(temperatura, vento, currentCode, currentProbPrecipitation, text!, isBadWeather))],
         maxTokens: 4096,
         temperature: 1,
         topP: 1,
